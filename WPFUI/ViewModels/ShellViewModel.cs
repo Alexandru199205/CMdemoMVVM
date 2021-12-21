d51f8152783288f2174432240677c477cf4db59d
bacescualexandru@yahoo.com <bacescualexandru@yahoo.com>
2021-12-21 13:20:33 +0200

    21.12.2021 mvvm.1

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -9,6 +9,15 @@ namespace WPFUI.ViewModels
 {
    public class ShellViewModel:Screen
     {
+        private string  _firstName;
+
+        public string FirstName
+        {
+            get
+            { return _firstName; }
+            set 
+            { _firstName = value; }
+        }
 
     }
 }
